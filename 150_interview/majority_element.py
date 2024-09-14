702f2614e4d0317d476c55198f95962e4add35d8
nandanpabolu <nandan.pabolu808@gmail.com>
2024-09-14 16:56:04 -0500

    this question is hard

---
+++ 150_interview/majority_element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
