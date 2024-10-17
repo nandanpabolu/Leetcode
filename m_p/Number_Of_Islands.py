21fe46fe59202354fe4439cc5ba91d1651d5f3a8
nandanpabolu <nandan.pabolu808@gmail.com>
2024-10-17 13:13:17 -0500

    new file added

---
+++ m_p/Number_Of_Islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        m = len(grid)
+        n = len(grid[0])
+
+        def zero(i,j):
+
+            if (i!=m and i!=-1 and j!=n and j!=-1 and  grid[i][j]=="1"):
+                grid[i][j]="0"
+                zero(i+1,j)
+                zero(i-1,j)
+                zero(i,j+1)
+                zero(i,j-1)
+            
+            return
+
+        total =0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    total+=1
+                    zero(i,j)
+
+        return total   
