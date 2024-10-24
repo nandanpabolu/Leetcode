857bd1b0ba5db2434906b70acd43a90ddde9cc44
nandanpabolu <nandan.pabolu808@gmail.com>
2024-10-24 18:49:29 -0500

    new code, playing around with BST

---
+++ m_p/426_Convert_BST_into_Double_Linked_List.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        self.first = None
+        self.last = None
+
+        def inorder(node):
+            
+            if not node:
+                return None
+
+            inorder(node.left)
+            if self.last:
+                self.last.right = node 
+                node.left = self.last
+            else:
+                self.first = node
+            self.last = node
+
+            inorder(node.right)
+        inorder(root)
+
+        if self.last and self.first:
+            self.first.left = self.last
+            self.last.right  = self.first
+        return self.first
